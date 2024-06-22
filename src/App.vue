af5e88611d83a13b5d8e7cf1f81014f849840dc9
axreer <saidaxrorpc7@gmail.com>
2024-06-22 20:37:34 +0500

    changed

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,54 @@
 <template>
+    <div class="container" v-if="weather">
+        <Navbar />
+        <Main />
+    </div>
 
-<div class="container" v-if="weather">
-<Navbar/>
-<Main/>
-
-</div>
-
-<div class="loading" v-else>
-  <h1>Идёт загрузка...</h1>
-
-</div>
-  
+    <div class="loading" v-else>
+        <h1>Идёт загрузка...</h1>
+    </div>
 </template>
 
 <script>
+    import Navbar from "./components/Navbar.vue";
+    import Main from "./components/Main.vue";
 
-import Navbar from './components/Navbar.vue'
-import Main from './components/Main.vue'
+    import { mapState, mapActions } from "vuex";
 
-import {mapState, mapActions} from 'vuex'
+    export default {
+        components: {
+            Navbar,
+            Main,
+        },
+        data() {
+            return {
+                weather: false,
+            };
+        },
+        mounted() {
+            setTimeout(() => {
+                this.weather = true;
+            }, 2500);
+        },
+        computed: {
+            ...mapState(["weather"]),
+        },
 
+        methods: {
+            ...mapActions(["getWeather"]),
+        },
 
-export default {
-    components: {
-      Navbar,
-      Main
-    },
- computed: {
-   ...mapState(['weather'])
- },
-
- methods: {
-  ...mapActions(['getWeather'])
- },
-
- created(){
-  this.getWeather('Ташкент')
- }
-
-
-}
+        created() {
+            this.getWeather("Ташкент");
+        },
+    };
 </script>
 
-<style scoped> 
-  .loading {
-    width: 100%;
-    height: 100vh;
-    display: grid;
-    place-content: center;
-
-  }
+<style scoped>
+    .loading {
+        width: 100%;
+        height: 100vh;
+        display: grid;
+        place-content: center;
+    }
 </style>
-
